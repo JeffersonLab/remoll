90a3ba2487512944cc3b68f6f9cba7e4785db1c5
Rupesh Silwal <silwalr@mit.edu>
2013-12-03 16:33:16 -0500

    Add collimator cuts such that events from det200_r<0.03m or det201_r<0.05m do not make it to the dets

--- src/remollEventAction.cc
+++ src/remollEventAction.cc
@@ -41,30 +41,55 @@ void remollEventAction::EndOfEventAction(const G4Event* evt ) {
 
   // Traverse all hit collections, sort by output type
   for( int hcidx = 0; hcidx < HCE->GetCapacity(); hcidx++ ){
-      thiscol = HCE->GetHC(hcidx);
-      if(thiscol){ // This is NULL if nothing is stored
-	  // Dyanmic cast to test types, process however see fit and feed to IO
-	  
-	  ////  Generic Detector Hits ///////////////////////////////////
-	  if( remollGenericDetectorHitsCollection *thiscast = 
-		  dynamic_cast<remollGenericDetectorHitsCollection *>(thiscol)){
-	      for( unsigned int hidx = 0; hidx < thiscast->GetSize(); hidx++ ){
-		  fIO->AddGenericDetectorHit(
-			  (remollGenericDetectorHit *) thiscast->GetHit(hidx) );
-	      }
-	  }
-	  
-	  ////  Generic Detector Sum ////////////////////////////////////
-	  if( remollGenericDetectorSumCollection *thiscast = 
-		  dynamic_cast<remollGenericDetectorSumCollection *>(thiscol)){
-	      for( unsigned int hidx = 0; hidx < thiscast->GetSize(); hidx++ ){
-		  fIO->AddGenericDetectorSum(
-			  (remollGenericDetectorSum *) thiscast->GetHit(hidx) );
-	      }
-	  }
+    thiscol = HCE->GetHC(hcidx);
+    if(thiscol){ // This is NULL if nothing is stored
+      // Dyanmic cast to test types, process however see fit and feed to IO
+      
+      Bool_t det200cut=1;
+      Bool_t det201cut=1;
 
+      ////  Generic Detector Hits ///////////////////////////////////
+      if( remollGenericDetectorHitsCollection *thiscast = 
+	  dynamic_cast<remollGenericDetectorHitsCollection *>(thiscol)){
+	for( unsigned int hidx = 0; hidx < thiscast->GetSize(); hidx++ ){
 
+	  remollGenericDetectorHit *currentHit = 
+	    (remollGenericDetectorHit *) thiscast->GetHit(hidx);
+
+	  //// flag hits from inside the inner radius of coll
+	  //   these events need to be discarded
+	  //   these could potentially end up in det, and contaminate data
+	  if(currentHit->fDetID==200 && currentHit->f3X.perp()/m<0.03)
+	    det200cut=0;
+	  
+	  if(currentHit->fDetID==201 && currentHit->f3X.perp()/m<0.05)
+	    det201cut=0;
+	}
       }
+
+      ////  Generic Detector Hits ///////////////////////////////////
+      if( remollGenericDetectorHitsCollection *thiscast = 
+	  dynamic_cast<remollGenericDetectorHitsCollection *>(thiscol)){
+	for( unsigned int hidx = 0; hidx < thiscast->GetSize(); hidx++ ){
+
+	  if(det200cut && det201cut)
+	    fIO->AddGenericDetectorHit((remollGenericDetectorHit *) 
+				       thiscast->GetHit(hidx) );	  
+	}
+      }
+      
+      ////  Generic Detector Sum ////////////////////////////////////
+      if( remollGenericDetectorSumCollection *thiscast = 
+	  dynamic_cast<remollGenericDetectorSumCollection *>(thiscol)){
+	for( unsigned int hidx = 0; hidx < thiscast->GetSize(); hidx++ ){
+
+	  if(det200cut && det201cut)
+	    fIO->AddGenericDetectorSum((remollGenericDetectorSum *) 
+				       thiscast->GetHit(hidx) );
+	}
+      }
+      
+    }
   }
 
   // Fill tree and reset buffers
