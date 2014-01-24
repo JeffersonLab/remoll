526369246c1bfb79266fd7648cb0b1d364f2efb0
Seamus Riordan <sriordan@physics.umass.edu>
2014-01-24 16:48:46 -0500

    rootlogon for all environments
    Conflicts:
    	rootlogon.C

--- rootlogon.C
+++ rootlogon.C
@@ -1,3 +1,9 @@
 void rootlogon(){
-    gSystem->Load("libremollroot.so");
+    FileStat_t buf;
+    if( !gSystem->GetPathInfo("libremollroot.so", buf) ){
+	gSystem->Load("libremollroot.so" ) ;
+    }
+    if( !gSystem->GetPathInfo("libremollroot.dylib", buf) ){
+	gSystem->Load("libremollroot.dylib" ) ;
+    }
 }
