6a18009021987a0de1fa98f9008c9151863da4aa
Rupesh Silwal <silwalr@mit.edu>
2014-02-14 09:04:50 -0500

    Limit rootfiles to 2GB. For rootfiles > 2GB, multiple rootfiles, each limited to 2GB is created

--- src/remollIO.cc
+++ src/remollIO.cc
@@ -56,6 +56,8 @@ void remollIO::InitializeTree(){
 
     fTree = new TTree("T", "Geant4 Moller Simulation");
 
+    fTree->SetMaxTreeSize(1900000000LL); // 1.9GB
+
     // Event information
     fTree->Branch("rate",     &fEvRate,   "rate/D");
     fTree->Branch("ev.A",     &fEvAsym,   "ev.A/D");
