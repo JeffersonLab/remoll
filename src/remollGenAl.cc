a3bf8755f182c3f7551351c94ca6103ec74b17b5
Ciprian Gal <ciprian@jlab.org>
2015-02-19 11:26:50 -0500

    improve error messages; exit on nan xsection; restrict Q2 to 0 10 GeV2 and W2 to 0 9

--- src/remollGenAl.cc
+++ src/remollGenAl.cc
@@ -51,7 +51,7 @@ void remollGenAl::SamplePhysics(remollVertex *vert, remollEvent *evt) {
 
     if( vert->GetMaterial()->GetNumberOfElements() != 1 ) {
         G4cerr << __FILE__ << " line " << __LINE__ <<
-               ": Error!  Some lazy programmer didn't account for complex materials in the moller process!" << G4endl;
+               "  : Error!  Some lazy programmer didn't account for complex materials in the moller process!" << G4endl;
         exit(1);
     }
 
@@ -75,24 +75,29 @@ void remollGenAl::GenInelastic(double beamE,double th,
     double W1 = 0.0;
     double xsect = 0.0;
 
-    // Generate flat energy distribution of outgoing electron
-    eOut =  electron_mass_c2 + CLHEP::RandFlat::shoot(electron_mass_c2, beamE);
-
-    double CTH = cos(th/2.0);
-    double STH = sin(th/2.0);
-    double T2THE = STH*STH/CTH/CTH;
-    double Nu = beamE - eOut;
-    Q2 = 4.0*beamE*eOut*STH*STH;
-    W2 = pow(proton_mass_c2,2) + 2.0*proton_mass_c2*Nu - Q2;// FIXME labeled Wsq
-
+    double CTH,STH,T2THE,Nu;
+    
+    do{
+      eOut   =  electron_mass_c2 + G4UniformRand()*(beamE - electron_mass_c2);// Generate flat energy distribution of outgoing electron
+      CTH    = cos(th/2.0);
+      STH    = sin(th/2.0);
+      T2THE  = STH*STH/CTH/CTH;
+      Nu     = beamE - eOut;
+      Q2     = 4.0*beamE*eOut*STH*STH;
+      W2     = pow(proton_mass_c2,2) + 2.0*proton_mass_c2*Nu - Q2;
+    }while(W2/GeV/GeV<0 || W2/GeV/GeV>9 || Q2/GeV/GeV<0 || Q2/GeV/GeV>10); //this is because F1F2IN09 won't work for W>3 and Q2>10
+    
     // Mott scattering
     double MOTT = pow((0.00072/beamE*CTH/STH/STH),2);
     MOTT = MOTT*1.0E4; // Units: ub/sr/GeV
 
-    int bad=F1F2IN09(13, 27, Q2/GeV/GeV, W2/GeV/GeV, F1, F2);  
+    int A=27;
+    int Z=13;
+    int bad=F1F2IN09(Z, A, Q2/GeV/GeV, W2/GeV/GeV, F1, F2);  
     if(bad){
       G4cerr << "ERROR: " << __FILE__ << " line " << __LINE__ << G4endl;	
-      G4cerr << "-1 = Q2,W2 out of range in resmodd | -2 = A<3" << bad << G4endl;
+      G4cerr << "  result was (-1 = Q2,W2 out of range in resmodd | -2 = A<3) : " << bad <<G4endl;
+      G4cerr << "     Q2 W2 A "<<Q2/GeV/GeV<<" "<<W2/GeV/GeV<<" "<<A<< G4endl;
       asym=0.99999;
       effectiveXsection=0;
       return;
@@ -100,18 +105,16 @@ void remollGenAl::GenInelastic(double beamE,double th,
     W1 = F1/proton_mass_c2;
  
     xsect = MOTT*(F2/(Nu/GeV) + 2.0*T2THE*(W1*GeV))*(beamE/GeV - electron_mass_c2/GeV);
-    std::cout<<beamE<<" "<<eOut<<std::endl;
-//     std::cout<<"Q2, Nu, W2, F1, F2, xsec "
-    std::cout<<std::setw(10)<<Q2/GeV/GeV<<" "<<std::setw(10)<<Nu/GeV<<" "<<std::setw(10)<<W2/GeV/GeV<<" "<<std::setw(10)<<F1<<" "<<std::setw(10)<<F2<<" "<<std::setw(10)<<xsect<<std::endl;
-    // In some cases a negative F2 is returned giving a negative cross section
-    if (xsect <= 0) {
+
+    if (xsect < 0 || std::isnan(xsect) ) {
         G4cerr << "ERROR: " << __FILE__ << " line " << __LINE__ << G4endl;	
-        G4cerr<<"Inelatic xsection < 0 "<<xsect<<" "<<F2
-              <<" "<<th<<" "<<Q2<<" "<<W2<<" "<<G4endl;
+        G4cerr<<"Inelatic xsection problem: "<<xsect<<" "<<F1<<" "<<F2
+              <<" "<<th<<" "<<Q2/GeV/GeV<<" "<<W2/GeV/GeV<<" "<<G4endl;
         asym=0.99999;
         effectiveXsection=0;
-        return;
+        exit(1);
     }
+
     asym=Q2*0.8e-4/GeV/GeV;
     fWeight = xsect*sin(th);
     effectiveXsection=xsect;
