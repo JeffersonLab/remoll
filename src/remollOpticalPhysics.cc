8d417a4720559d213ca6a0b69d38d86cb4158f75
Rupesh Silwal <silwalr@mit.edu>
2015-07-19 22:24:41 -0400

    Changed theParticleIterator to aParticleIterator, to be consistent with the nomenclature of newer versiosn of g4

--- src/remollOpticalPhysics.cc
+++ src/remollOpticalPhysics.cc
@@ -82,10 +82,10 @@ void remollOpticalPhysics::ConstructProcess()
   G4EmSaturation* emSaturation = G4LossTableManager::Instance()->EmSaturation();
   theScintProcess->AddSaturation(emSaturation);
 
-  theParticleIterator->reset();
-  while ( (*theParticleIterator)() ){
+  aParticleIterator->reset();
+  while ( (*aParticleIterator)() ){
 
-    G4ParticleDefinition* particle = theParticleIterator->value();
+    G4ParticleDefinition* particle = aParticleIterator->value();
     G4String particleName = particle->GetParticleName();
 
     pManager = particle->GetProcessManager();
