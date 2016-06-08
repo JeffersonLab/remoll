1401ad8f06af5d4ca6f304ae2162d0be4dbab924
Wouter Deconinck <wdconinc@gmail.com>
2016-06-08 16:13:34 -0400

    New class for LUND event generator.

---
+++ include/remollGenLUND.hh
@@ -0,0 +1,46 @@
+#ifndef __REMOLLGENLUND_HH 
+#define __REMOLLGENLUND_HH 
+/*!
+ * event generator based on LUND files
+ *
+ * Rakitha Beminiwattha
+ * Fri Apr 15 12:51:25 EDT 2016
+ *
+ */
+
+// remoll includes
+#include "remollVEventGen.hh"
+
+// System includes
+#include <map>
+#include <set>
+
+class remollRunData;
+
+class remollGenLUND : public remollVEventGen {
+
+  public:
+    // constructor
+    remollGenLUND();
+
+    // virtual destructor
+    virtual ~remollGenLUND();
+
+    void SetParticleType(G4String t) { fParticleType = t; }
+    void SetLUNDFile(G4String f);
+
+  private:
+    void SamplePhysics(remollVertex *, remollEvent *);
+
+    G4String fParticleType;
+
+    remollRunData *fRunData;
+
+    std::ifstream LUNDfile;
+
+    G4bool bLUND;
+    std::map<int,G4String> pidname;
+
+};
+
+#endif//__REMOLLGENLUND_HH 
