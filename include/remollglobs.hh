123e0fd53790cb0535a6f83a624c3751cc3d5964
yxzhao88 <yuxiang.zhao@stonybrook.edu>
2016-06-28 14:15:49 -0400

    Add files via upload
    change the sign of QWe and QWp

--- include/remollglobs.hh
+++ include/remollglobs.hh
@@ -15,10 +15,10 @@ const double alpha = 0.007299;
 //const double sin2thW_ms = 0.23116; // PDG 2012
 const double sin2thW_ms = 0.23125; // arxiv 1302.6263 - KK's low Q2 EW review
 
-const double QWe =  0.0435;  // arxiv 1302.6263 - KK's low Q2 EW review
-const double QWe_rad = 0.002787;  //  arxiv 1302.6263 - KK's low Q2 EW review with radiative (eq 31)
+const double QWe =  -0.0435;  // arxiv 1302.6263 - KK's low Q2 EW review
+const double QWe_rad = -0.002787;  //  arxiv 1302.6263 - KK's low Q2 EW review with radiative (eq 31)
 	                     //  See HAPLOG 284
-const double QWp = -0.0707;
+const double QWp = 0.0707;
 
 const double gDefaultBeamE   = 11.0*GeV;
 const double gDefaultBeamPol = 0.85;
