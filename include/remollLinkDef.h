55ebe569b06981687d8eae8b1cd3c2e65d81c38d
Wouter Deconinck <wdconinc@gmail.com>
2017-02-10 17:33:12 -0500

    Make sure dictionaries are created for new types.

--- include/remollLinkDef.h
+++ include/remollLinkDef.h
@@ -8,5 +8,11 @@
 #pragma link C++ class remollTextFile+;
 #pragma link C++ struct filedata_t+;
 
+#pragma link C++ struct remollGenericDetectorHit_t+;
+#pragma link C++ class vector<remollGenericDetectorHit_t>+;
+
+#pragma link C++ struct remollGenericDetectorSum_t+;
+#pragma link C++ class vector<remollGenericDetectorSum_t>+;
+
 #endif
 
