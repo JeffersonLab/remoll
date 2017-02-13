09e879f2ba0f0273a0d71c164a23e74854c6f7fe
Wouter Deconinck <wdconinc@gmail.com>
2017-02-13 14:04:07 -0500

    Move additional remoll system of units and physical constants into a separate header, which is still included through remollglobs.hh.

---
+++ include/remollSystemOfUnits.hh
@@ -0,0 +1,11 @@
+#ifndef __REMOLLSYSTEMOFUNITS_HH
+#define __REMOLLSYSTEMOFUNITS_HH
+
+#include "G4Types.hh"
+#include "G4SystemOfUnits.hh"
+
+// New units
+static const G4double ppm = 1E-6;
+static const G4double ppb = 1E-9;
+
+#endif // __REMOLLSYSTEMOFUNITS_HH
