365e0c61144fc64f9878e0144baf8090366f255d
Wouter Deconinck <wdconinc@gmail.com>
2017-02-22 11:03:41 -0500

    Removed remoll.C since not a solution that can be used for multiple analysis.
    Now, my_analysis is the framework of an analysis script that can be copied
    many times and modified as necessary without stepping on other code.
    
    re-root [] .L analysis/my_analysis.C+
    re-root [] my_analysis a;
    re-root [] a.Loop()
    re-root [] h_hit_n->Draw();
    re-root [] h_hit_pid->Draw();
    
    my_analysis is an example analysis object that inherits from remoll. The
    pure virtual Loop function must be implemented by the analysis object.
    Many different analysis objects can be generated by copying my_analysis.C
    to different filenames and renaming the class (and even that is not really
    necessary). Analysis objects can even have their own inheritance structure
    (as long as you call Loop() properly and are OK with multiple loops through
    the tree.

--- analysis/remoll.h
+++ analysis/remoll.h
@@ -35,7 +35,7 @@ public :
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
    virtual void     Init(TTree *tree);
-   virtual void     Loop();
+   virtual void     Loop() = 0;
    virtual Bool_t   Notify();
    virtual void     Show(Long64_t entry = -1);
 };
