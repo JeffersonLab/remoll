e9c7e0c4fee4e0d3e9449a664860ffb7e4564ad7
Wouter Deconinck <wdconinc@gmail.com>
2017-03-10 16:50:50 -0500

    Little script to create random samplings of GDML files.
    How do you encode the uncertainty present in the geometry description of
    an experiment? A systematic study of all geometric variables by scanning
    over them in a linear fashion becomes quite cumbersome. So, here we take
    a probabilistic many-worlds approach: we encode the uncertainty in the
    gdml file and run a pre-processor to generate a random sampling of many
    gdml files that is consistent with a 67% confidence level, given the
    uncertainties encoded in the files.
    
    But really, I just wanted a way to quickly create a number of geometries
    that vary in certain parameters (uniformly).
    
    How to use this? Use the following syntax anywhere in your files:
      random_normal(mean,sigma)
    with values for mean and sigma. They will be replaced by a normally
    distributed value when sample_gdml.sh runs over it. Also works with
      random_uniform(mean,half)
    where half is half the range (i.e. -1.0 -- +1.0 has half = 1.0).
    
    You can specify a file or directory. The preprocessed file or directory
    will be placed in a new directory with randomized name.
    
    You can also pipe a stream into this command and it will return a stream
    for you to redirect somewhere.
    
    Works not just on gdml files, of course.

---
+++ scripts/sample_gdml.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# libmath-random-perl required
+
+# Sample stream
+sample_stream ()
+{
+  # Read lines
+  while read line ; do
+    # Do nothing, just pass through
+    # here be implementation details
+
+    # floating point regex
+    fl="[-+]?[0-9]*\.?[0-9]+"
+    # gdml parameter regex
+    re="random_distribution\(($fl),($fl)\)"
+
+    # Gaussian: "random_normal(mean,sigma)"
+    thisre=${re/distribution/normal}
+    if [[ "$line" =~ $thisre ]] ; then
+      mean=${BASH_REMATCH[1]}
+      sigma=${BASH_REMATCH[2]}
+
+      value=`perl -e "use Math::Random; print Math::Random::random_normal(1,$mean,$sigma);"`
+      echo "$line" | sed -r "s/$thisre/$value/"
+      continue
+    fi
+
+    # Uniform: "random_uniform(mean,half)"
+    thisre=${re/distribution/uniform}
+    if [[ "$line" =~ $thisre ]] ; then
+      mean=${BASH_REMATCH[1]}
+      half=${BASH_REMATCH[2]}
+
+      value=`perl -e "use Math::Random; print Math::Random::random_uniform(1,$mean-$half,$mean+$half);"`
+      echo "$line" | sed -r "s/$thisre/$value/"
+      continue
+    fi
+
+    # Nothing recognized
+    echo "$line"
+  done
+}
+
+
+sample_files () {
+  # Loop over arguments
+  while [ $# -gt 0 ] ; do
+    arg=$1
+    shift
+
+    # Check if file exists
+    if [ ! -e $arg ] ; then
+      echo "skipping non-existing entry $arg"
+      continue
+    fi
+
+    # Recurse into directories
+    if [ -d $arg ] ; then
+      sample_files $arg/*
+      continue
+    fi
+
+    # Process files
+    if [ -f $arg ] ; then
+      # Sample arg as stream
+      echo "sampling $arg..."
+      mkdir -p `dirname $DIR/$arg`
+      cat $arg | sample_stream > $DIR/$arg
+      continue
+    fi
+
+    # Unknown
+    echo "what's with $arg?"
+
+  done
+}
+
+# Read from stdin and output to stdout if $# -eq 0
+if [ $# -eq 0 ] ; then
+  sample_stream
+# Else parse files one by one
+else
+  # Determine output directory
+  DIR=`mktemp -d sample_gdml.XXXXXX`
+  echo "output in $DIR"
+  sample_files $*
+fi
