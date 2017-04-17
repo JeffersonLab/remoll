02cf527c23d45547748b673ba75e272fc68b6cb1
Wouter Deconinck <wdconinc@gmail.com>
2017-04-17 10:48:24 -0400

    Parallel world support.
    What are parallel worlds (good for)? In geant4 they are non-material
    worlds that can be used for sensitive detectors etc. The volumes in
    the parallel world can overlap arbitrarily with material volumes in
    the real world (but they can't overlap with themselves, just like any
    geometry). We can use them for virtual detectors, if that makes sense,
    or we can use them just for visualization aspects. I've taken the
    second route here by adding a cone to represent the ee scattering
    sheet in the pion detector region. One could expand this to other
    backgrounds etc. This is plainly a visualization solution, no physics
    is changed by this.
    
    Caveat: there's no messenger to set the parallel world gdml, so for
    now only parallelWorld.gdml is supported.

---
+++ src/remollParallelWorldConstruction.cc
@@ -0,0 +1,41 @@
+#include "remollParallelWorldConstruction.hh"
+
+#include <G4GDMLParser.hh>
+#include <G4LogicalVolume.hh>
+#include <G4PVPlacement.hh>
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+remollParallelWorldConstruction
+::remollParallelWorldConstruction(G4String& parallelWorldName)
+: G4VUserParallelWorld(parallelWorldName),
+  fConstructed(false),fFileName("geometry_sculpt/parallelWorld.gdml")
+{;}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+remollParallelWorldConstruction::~remollParallelWorldConstruction()
+{;}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+void remollParallelWorldConstruction::Construct()
+{
+  if (fConstructed) return;
+  fConstructed = true;
+
+  // Read GDML parallel world
+  G4GDMLParser* parser = new G4GDMLParser();
+  parser->Clear();
+  parser->Read(fFileName);
+
+  // Get parallel world
+  G4VPhysicalVolume* parallelWorld = parser->GetWorldVolume();
+  G4LogicalVolume *parallelWorldLogical = parallelWorld->GetLogicalVolume();
+
+  // Get ghost world
+  G4VPhysicalVolume* ghostWorld = GetWorld();
+  G4LogicalVolume* ghostWorldLogical = ghostWorld->GetLogicalVolume();
+
+  // Place parallel world into ghost world
+  new G4PVPlacement(0,G4ThreeVector(0.,0.,0.),
+                    parallelWorldLogical,"parallelWorld",
+                    ghostWorldLogical,false,0);
+}
