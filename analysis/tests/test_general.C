f007dc5d52a555df1af9adb0ad46cdeb24000363
Wouter Deconinck <wdconinc@gmail.com>
2017-07-17 11:41:42 -0400

    Explicitly use moller evgen for test_raster macro
    The test_raster macro really only tests whether the raster works
    properly. test_general.C therefore now creates a few more plots.
    However, if no event generator is explicitly set in the macro,
    it is not clear what event generator is actually used. In the MT
    branch this default is set to the moller generator. Here the macro
    is updated to explicitly set the test_raster generator to the moller
    event generator as well. This differs from earlier default behavior
    in master and v1.0.0 (which is somewhat ill-defined), but agrees
    with future default behavior (no event generator specified -> moller).

--- analysis/tests/test_general.C
+++ analysis/tests/test_general.C
@@ -23,6 +23,12 @@ void test_general (const TString& inputdir, const TString& inputname)
   c1.SaveAs(outputdir + "/" + inputname + "_" + "h_hit_det.png");
   T.Draw("hit.r   >> h_hit_r(150,0.0,1.5)","hit.det==28",hopt);
   c1.SaveAs(outputdir + "/" + inputname + "_" + "h_hit_r.png");
+  T.Draw("ev.vx >> h_vx(200,-0.010,+0.010)","",hopt);
+  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_vx.png");
+  T.Draw("ev.vy >> h_vy(200,-0.010,+0.010)","",hopt);
+  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_vy.png");
+  T.Draw("ev.vz >> h_vz(200,-1.00,+1.00)","",hopt);
+  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_vz.png");
 
   // Save and close
   file.Write();
