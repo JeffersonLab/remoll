b79c927fc82a94dc6f3adafc8039dc57cfec9540
Wouter Deconinck <wdconinc@gmail.com>
2017-08-23 13:38:28 -0400

    Rewrite of most of the test suite framework.
    To run tests, run scripts/tests/test_macros.sh with three arguments
    test_macros.sh [suite = commit] [macroglob = *.mac] [analysisglob = *.root]
    
    For example, `test_macros.sh valgrind` will run scripts through valgrind,
    and `test_macros.sh unit` will run a suite of unit tests in macros/tests/unit

--- scripts/tests/test_macros.sh
+++ scripts/tests/test_macros.sh
@@ -1,27 +1,97 @@
 #!/bin/bash
 
+# Determine absolute path of this script
 dir=`dirname $0`/../..
 dir=`readlink -f ${dir}`
 
-rootfiles=${dir}/rootfiles/tests
-analysis=${dir}/analysis/tests
-logfiles=${dir}/logfiles/tests
-macros=${dir}/macros/tests
-mkdir -p ${logfiles}
+# The test suite can be specified as first argument, default is "commit"
+suite="${1:-commit}"
+
+# A specific remoll macro can be specified as second argument, default is "*.mac"
+shopt -s nullglob
+macroglob="${2:-*.mac}"
+
+# A specific analysis macro can be specified as second argument, default is "*.root"
+shopt -s nullglob
+analysisglob="${3:-*.C}"
+
+# Set test suite input directories
+macros=${dir}/macros/tests/${suite}
+analysis1=${dir}/analysis/tests
+analysis2=${dir}/analysis/tests/${suite}
+
+# Set test suite output directories
+rootfiles=${dir}/rootfiles/tests/${suite}
+logfiles=${dir}/logfiles/tests/${suite}
+mkdir -p ${logfiles} ${rootfiles} ${rootfiles}/analysis
 
 
-mkdir -p ${rootfiles}
-for macro in ${macros}/*.mac ; do
+# Usage information
+if [ $# -eq 0 ] ; then
+	echo "Usage: `basename $0` [suite = commit] [macroglob = *.mac] [analysisglob = *.root]"
+	echo
+fi
+echo "This script will run a test suite with files located in the following directories:"
+echo " remoll macros in ${macros}/${macroglob}"
+echo " analysis macros in ${analysis1}/${analysisglob}"
+echo " analysis macros in ${analysis2}/${analysisglob}"
+echo
+
+
+# If test suite is "valgrind"
+prefix=""
+if [ "$suite" == "valgrind" ] ; then
+	prefix="valgrind $suppression"
+
+	# Figure out ROOT valgrind suppression
+	suppression=""
+	if [ -f "/etc/root/valgrind-root.supp" ] ; then
+	        suppression="--suppressions=/etc/root/valgrind-root.supp"
+	fi
+	if [ -f "$ROOTSYS/etc/valgrind-root.supp" ] ; then
+	        suppression="--suppressions=$ROOTSYS/etc/valgrind-root.supp"
+	fi
+	echo "Valgrind suppression options: $suppression"
+	echo
+fi
+
+
+# Run test suite macros as requested
+for macro in ${macros}/${macroglob} ; do
+
 	name=`basename ${macro} .mac`
-	echo "Running ${name}..."
-	build/remoll ${macro} 2>&1 | tee ${logfiles}/${name}.log
-done
 
+	# Announce
+	echo "Running remoll macro `basename ${macro} .mac`..."
 
-mkdir -p ${rootfiles}/analysis
-for rootfile in ${rootfiles}/*.root ; do
-	name=`basename ${rootfile} .root`
-	for rootmacro in ${analysis}/*.C ; do
+	# Run remoll macro
+	mkdir -p ${logfiles}
+	$prefix build/remoll ${macro} 2>&1 | tee ${logfiles}/${name}.log
+
+	# Unit tests do not have output
+	if [ "$suite" == "unit" ] ; then
+		continue
+	fi
+
+	# Determine output file
+	if [ -f ${name}.root ] ; then
+		mkdir -p ${rootfiles}
+		rootfile=${rootfiles}/${name}.root
+		mv -f ${name}.root ${rootfile}
+	else
+	        echo "Output file ${name}.root not found."
+	        continue
+	fi
+
+	# Analyze output file
+	mkdir -p ${rootfiles}/analysis
+	for rootmacro in ${analysis1}/${analysisglob} ; do
+		echo "Running analysis macro `basename ${rootmacro} .C`..."
 		root -q -b -l "${rootmacro}+(\"${rootfiles}\",\"${name}\")" 2>&1 | tee -a ${logfiles}/${name}.log
 	done
+	for rootmacro in ${analysis2}/${analysisglob} ; do
+		echo "Running analysis macro `basename ${rootmacro} .C`..."
+		root -q -b -l "${rootmacro}+(\"${rootfiles}\",\"${name}\")" 2>&1 | tee -a ${logfiles}/${name}.log
+	done
+
 done
