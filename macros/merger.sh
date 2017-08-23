2398e0fd4367d5893c25da84b6cbf5cdb6813fd8
Cameron Clarke <cameronc137@gmail.com>
2017-08-23 10:54:10 -0400

    tiny update

--- macros/merger.sh
+++ macros/merger.sh
@@ -4,7 +4,8 @@ dir=$HOME/gitdir/dose_remoll/build/output
 i=2
 while [ $i -le 10 ];
 do
-	hadd $dir/result.root $dir/out$i/remoll_beam_tracking_1M.root $dir/Result.root
+  #destination, add in, add original
+	hadd $dir/result.root $dir/out_AlCan$i/remoll_beam_tracking_1M_NEW.root $dir/Result.root
 	let i=i+1
 	mv $dir/result.root $dir/Result.root
 done
