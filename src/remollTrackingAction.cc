eacd795bb9553ab39ef7c1872d442901b6057979
Wouter Deconinck <wdconinc@gmail.com>
2018-05-05 10:45:06 +0200

    Should have kept fTrackingFlag default = 3, track all particles, fixed

--- src/remollTrackingAction.cc
+++ src/remollTrackingAction.cc
@@ -6,7 +6,7 @@
 #include "G4GenericMessenger.hh"
 
 remollTrackingAction::remollTrackingAction()
-: fMessenger(0)
+: fMessenger(0),fTrackingFlag(3)
 {
   fMessenger = new G4GenericMessenger(this,"/remoll/tracking/","Remoll tracking properties");
   fMessenger->DeclareProperty("set",fTrackingFlag)
@@ -28,50 +28,37 @@ remollTrackingAction::~remollTrackingAction()
 void remollTrackingAction::PreUserTrackingAction(const G4Track* aTrack)
 {
   // Track primary electron only
-  if (fTrackingFlag == 0)
-    {
-	if( aTrack->GetParentID()==0 )
-	{
-	}
-	else
-	{
-            fpTrackingManager->EventAborted();
-            return;
-	}
+  if (fTrackingFlag == 0) {
+    if (aTrack->GetParentID() == 0) {
+    } else {
+        fpTrackingManager->EventAborted();
+      return;
     }
+  }
 
   // Track primary electron and optical photon
-  else if (fTrackingFlag == 1)
+  else if (fTrackingFlag == 1) {
+    if ((aTrack->GetParentID() == 0) || (aTrack->GetDefinition() == G4OpticalPhoton::OpticalPhotonDefinition()))
     {
-	if( (aTrack->GetParentID()==0) || (aTrack->GetDefinition() == G4OpticalPhoton::OpticalPhotonDefinition()) )
-	{
-	}
-	else
-	{
-            fpTrackingManager->EventAborted();
-            return;
-	}
-
+    } else {
+      fpTrackingManager->EventAborted();
+      return;
     }
+  }
 
   // Track primary electron and secondaries except optical photon
-  else if (fTrackingFlag == 2)
+  else if (fTrackingFlag == 2) {
+    if (aTrack->GetDefinition() != G4OpticalPhoton::OpticalPhotonDefinition())
     {
-	if( aTrack->GetDefinition() != G4OpticalPhoton::OpticalPhotonDefinition() )
-	{
-	}
-	else
-	{
-            fpTrackingManager->EventAborted();
-            return;
-	}
-
+    } else {
+      fpTrackingManager->EventAborted();
+      return;
     }
+  }
 
   // Track all particles
-  else
-    {
-    }
+  else {
+  }
 }
 
 void remollTrackingAction::PostUserTrackingAction(const G4Track* /*aTrack*/) { }
