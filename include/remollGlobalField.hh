1eb4fd5848dae454992f6ff8ca0aa9c67af5b3d2
Wouter Deconinck <wdconinc@gmail.com>
2018-05-14 18:07:01 -0400

    Implemented possibility of turning on spin precession tracking
    In the magnetic field the spin of polarized electrons will precess and
    may end up being transverse at the detector plane. In MOLLER we should
    not be sensitive to the effects of this, but it has been on my todo list
    to implement a way to check this.
    
    /remoll/field/equationtype
      Set equation type:
       0: B-field, no spin (default)
       1: EM-field, no spin
       2: B-field, with spin
       3: EM-field, with spin
    
    /remoll/field/steppertype
      Set stepper type:
       0: ExplicitEuler
       1: ImplicitEuler
       2: SimpleRunge
       3: SimpleHeum
       4: ClassicalRK4 (default)
       5: CashKarpRKF45
    
    /remoll/field/print
      Print the accuracy parameters

--- include/remollGlobalField.hh
+++ include/remollGlobalField.hh
@@ -11,13 +11,22 @@
 #include <vector>
 
 class G4GenericMessenger;
+class G4EquationOfMotion;
+class G4MagIntegratorStepper;
+class G4ChordFinder;
+class G4FieldManager;
+class G4PropagatorInField;
 
 class remollMagneticField;
 
 class remollGlobalField : public G4MagneticField {
-    public: 
+    public:
         remollGlobalField();
 	virtual ~remollGlobalField();
+
+        /// GetObject() returns the single remollGlobalField object.
+        /// It is constructed, if necessary.
+        static remollGlobalField* GetObject();
 
 	void AddNewField(G4String& name);
 	void SetFieldScaleByString(G4String& name_scale);
@@ -27,12 +36,72 @@ class remollGlobalField : public G4MagneticField {
 
 	void GetFieldValue(const G4double[], G4double*) const;
 
+    public:
+        /// Set the stepper types
+        void SetEquationType(G4int i) { fEquationType = i; SetEquation(); }
+
+        /// Set the stepper
+        void SetEquation();
+
+        /// Set the stepper types
+        void SetStepperType(G4int i) { fStepperType = i; SetStepper(); }
+
+        /// Set the stepper
+        void SetStepper();
+
+        /// Set the minimum step length
+        void SetMinStep(G4double stp) { fMinStep = stp; SetAccuracyParameters(); }
+
+        /// Set the delta chord length
+        void SetDeltaChord(G4double dcr) { fDeltaChord = dcr; SetAccuracyParameters(); }
+
+        /// Set the delta one step length
+        void SetDeltaOneStep(G4double stp) { fDeltaOneStep = stp; SetAccuracyParameters(); }
+
+        /// Set the delta intersection length
+        void SetDeltaIntersection(G4double its) { fDeltaIntersection = its; SetAccuracyParameters(); }
+
+        /// Set the minimum eps length
+        void SetEpsMin(G4double eps) { fEpsMin = eps; SetAccuracyParameters(); }
+
+        /// Set the maximum eps length
+        void SetEpsMax(G4double eps) { fEpsMax = eps; SetAccuracyParameters(); }
+
+        /// Set the accuracy parameters
+        void SetAccuracyParameters();
+
+        /// Print the accuracy parameters
+        void PrintAccuracyParameters();
+
+    private:
+        G4int fEquationType;
+        G4int fStepperType;
+
+        G4double fMinStep;
+        G4double fDeltaChord;
+        G4double fDeltaOneStep;
+        G4double fDeltaIntersection;
+        G4double fEpsMin;
+        G4double fEpsMax;
+
+        G4EquationOfMotion*     fEquation;
+	G4int                   fEquationDoF;
+
+        G4FieldManager*         fFieldManager;
+        G4PropagatorInField*    fFieldPropagator;
+        G4MagIntegratorStepper* fStepper;
+        G4ChordFinder*          fChordFinder;
+
     private:
 	std::vector<remollMagneticField*> fFields;
 
 	remollMagneticField* GetFieldByName(const G4String& name);
 
 	G4GenericMessenger* fMessenger;
+	G4GenericMessenger* fGlobalFieldMessenger;
+
+    private:
+        static G4ThreadLocal remollGlobalField* fObject;
 };
 
 
