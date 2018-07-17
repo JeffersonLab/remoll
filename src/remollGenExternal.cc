b762a83614f76dc38335e074eb0d3c9436e7a247
Wouter Deconinck <wdconinc@gmail.com>
2018-07-17 16:53:28 -0400

    Use correct units in remollGenExternal

--- src/remollGenExternal.cc
+++ src/remollGenExternal.cc
@@ -101,10 +101,10 @@ void remollGenExternal::SamplePhysics(remollVertex *vert, remollEvent *evt)
     }
 
     // Weighting completely handled by event file
-    evt->SetEffCrossSection(fEvent->xs);
+    evt->SetEffCrossSection(fEvent->xs*microbarn);
     evt->SetQ2(fEvent->Q2);
     evt->SetW2(fEvent->W2);
-    evt->SetAsymmetry(fEvent->A);
+    evt->SetAsymmetry(fEvent->A*ppb);
 
     // Loop over all hits in this event
     for (size_t i = 0; i < fHit->size(); i++) {
