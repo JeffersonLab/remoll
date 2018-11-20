5bdbca83c5eb6cd4e69b2f32ee0e1bc1da145340
Cameron Clarke <cameronc137@gmail.com>
2018-11-20 00:14:13 -0500

    making Findremoll -> FindEXE

--- cmake/modules/FindEXE.cmake
+++ cmake/modules/FindEXE.cmake
@@ -1,12 +1,12 @@
 # - Find remoll library
 # This module checks if remoll was compiled
 # It defines:
-# remoll_FOUND               If the remoll is found
+# exe_FOUND               If the remoll is found
 
-find_program(remoll NAMES remoll
+find_program(exe NAMES remoll
              PATHS ${PROJECT_BINARY_DIR}/remoll)
-if(remoll)
-    set(remoll_FOUND TRUE)
+if(exe)
+    set(exe_FOUND TRUE)
 else()
   set(REMOLL_FOUND FALSE)
   message(STATUS "NOT Found remoll: set REMOLL_INSTALL env var.")
