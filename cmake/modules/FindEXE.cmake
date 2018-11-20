adb82d3ec145fc03dae88afb19e12ca460ff3eb1
Cameron Clarke <cameronc137@gmail.com>
2018-11-20 00:23:27 -0500

    I said "probably" in the last commit because I have a bad habit of committing results before I test them...

--- cmake/modules/FindEXE.cmake
+++ cmake/modules/FindEXE.cmake
@@ -1,14 +1,14 @@
 # - Find remoll library
 # This module checks if remoll was compiled
 # It defines:
-# exe_FOUND               If the remoll is found
+# EXE_FOUND               If the remoll is found
 
-find_program(exe NAMES remoll
+find_program(EXE NAMES remoll
              PATHS ${PROJECT_BINARY_DIR}/remoll)
-if(exe)
-    set(exe_FOUND TRUE)
+if(EXE)
+  set(EXE_FOUND TRUE)
 else()
-  set(REMOLL_FOUND FALSE)
+  set(EXE_FOUND FALSE)
   message(STATUS "NOT Found remoll: set REMOLL_INSTALL env var.")
 
 endif()
