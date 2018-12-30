fe0a03ce52e6e01535cbe30ed53a43bf5ebde5d9
Ciprian Gal <ciprian@jlab.org>
2018-12-29 22:09:10 -0500

    i like my file sizes to be large

--- src/remollIO.cc
+++ src/remollIO.cc
@@ -80,7 +80,7 @@ void remollIO::InitializeTree()
 
     fTree = new TTree("T", "Geant4 Moller Simulation");
 
-    fTree->SetMaxTreeSize(1900000000); // 1.9GB
+    fTree->SetMaxTreeSize(8900000000); // 1.9GB
 
     // Event information
     fTree->Branch("rate",     &fEvRate,   "rate/D");
