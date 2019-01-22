e7c8463e9a5ef0f6d2b8c42db987694a1f7275f9
Wouter Deconinck <wdconinc@gmail.com>
2019-01-22 13:08:14 -0500

    Refactor of detector sum by_pid using maps

--- src/remollGenericDetectorSum.cc
+++ src/remollGenericDetectorSum.cc
@@ -3,65 +3,24 @@
 G4ThreadLocal G4Allocator<remollGenericDetectorSum>* remollGenericDetectorSumAllocator = 0;
 
 remollGenericDetectorSum::remollGenericDetectorSum(int detid, int copyid)
-: fDetID(detid),fCopyID(copyid) {
-  fEdep   = 0.0;
-
-  // Particles that are payed attention to in our sums, 0 means all types
-  std::vector<int> particles_to_track;
-  particles_to_track.push_back(0);
-  particles_to_track.push_back(-11);
-  particles_to_track.push_back( 11);
-  particles_to_track.push_back( 22);
-  particles_to_track.push_back(2112);
-  particles_to_track.push_back(2212);
-  particles_to_track.push_back(-211);
-  particles_to_track.push_back( 211);
-}
+: fDetID(detid),fCopyID(copyid),fEdep(0.0) { }
 
 remollGenericDetectorSum::~remollGenericDetectorSum() { }
 
-void remollGenericDetectorSum::AddEDep(int pid, G4ThreeVector pos, double ene)
+void remollGenericDetectorSum::AddEDep(int pid, G4ThreeVector pos, double edep)
 {
-    remollGenericDetectorSumByPID_t sum_by_pid;
+  fEdep += edep;
 
-    sum_by_pid.edep = ene;
-    sum_by_pid.x    = pos.x();
-    sum_by_pid.y    = pos.y();
-    sum_by_pid.z    = pos.z();
-    sum_by_pid.pid  = pid;
+  if (fSumByPID.count(pid) == 0)
+    fSumByPID.at(pid) = {.x = 0, .y = 0, .z = 0, .edep = 0, .pid = pid};
 
-    fSumByPID.push_back(sum_by_pid);
-}
+  G4double oldedep = fSumByPID[pid].edep;
+  fSumByPID[pid].edep += edep;
+  G4double newedep = fSumByPID[pid].edep;
 
-double remollGenericDetectorSum::GetEdep(int pid) const
-{
-    double esum = 0.0;
-
-    for (std::vector<remollGenericDetectorSumByPID_t>::const_iterator it = fSumByPID.begin() ; it != fSumByPID.end(); ++it){
-	if( pid==0 || (*it).pid == pid ){
-	    esum += (*it).edep;
-	}
-    }
-
-    return esum;
-}
-
-G4ThreeVector remollGenericDetectorSum::GetPos(int pid) const
-{
-    double esum = 0.0;
-    double xsum, ysum, zsum;
-    xsum = ysum = zsum = 0.0;
-
-    for (std::vector<remollGenericDetectorSumByPID_t>::const_iterator it = fSumByPID.begin() ; it != fSumByPID.end(); ++it){
-	if( pid==0 || (*it).pid == pid ){
-	    xsum += (*it).x*(*it).edep;
-	    ysum += (*it).y*(*it).edep;
-	    zsum += (*it).z*(*it).edep;
-	    esum += (*it).edep;
-	}
-    }
-
-    return G4ThreeVector(xsum/esum, ysum/esum, zsum/esum);
+  fSumByPID[pid].x = (oldedep * fSumByPID[pid].x + edep * pos.x()) / newedep;
+  fSumByPID[pid].y = (oldedep * fSumByPID[pid].y + edep * pos.y()) / newedep;
+  fSumByPID[pid].z = (oldedep * fSumByPID[pid].z + edep * pos.z()) / newedep;
 }
 
 remollGenericDetectorSum::remollGenericDetectorSum(const remollGenericDetectorSum &right)
