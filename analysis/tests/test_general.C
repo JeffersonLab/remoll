37b23e6784c26e18e5428ad3c5f809b9449e701d
Wouter Deconinck <wdconinc@gmail.com>
2019-03-18 11:57:10 -0400

    Save as pdf instead of png

--- analysis/tests/test_general.C
+++ analysis/tests/test_general.C
@@ -18,25 +18,25 @@ void test_general (const TString& inputdir, const TString& inputname)
 
   TCanvas c1;
   T.Draw("hit.pid >> h_hit_pid(5000,-2500,+2500)","",hopt);
-  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_hit_pid.png");
+  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_hit_pid.pdf");
   T.Draw("hit.det >> h_hit_det(4500,0,4500)","",hopt);
-  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_hit_det.png");
+  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_hit_det.pdf");
   T.Draw("hit.r/m >> h_hit_r(150,0.0,1.5)","hit.det==28",hopt);
-  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_hit_r.png");
+  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_hit_r.pdf");
   T.Draw("part.vx/m >> h_vx(200,-0.010,+0.010)","",hopt);
-  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_vx.png");
+  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_vx.pdf");
   T.Draw("part.vy/m >> h_vy(200,-0.010,+0.010)","",hopt);
-  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_vy.png");
+  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_vy.pdf");
   T.Draw("part.vz/m >> h_vz(200,-1.00,+1.00)","",hopt);
-  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_vz.png");
+  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_vz.pdf");
   T.Draw("ev.beamp/GeV >> h_beamp(110,0.0,+11.0)","",hopt);
-  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_beamp.png");
+  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_beamp.pdf");
   T.Draw("ev.beamp/GeV:part.vz/m >> h_beamp_vz(20,-1.0,+1.0)","",hopt+",PROF");
-  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_beamp_vz.png");
+  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_beamp_vz.pdf");
   T.Draw("ev.thcom/deg >> h_thcom(180,0.0,+180.0)","",hopt);
-  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_thcom.png");
+  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_thcom.pdf");
   T.Draw("log(rate) >> h_lograte(200,-25.0,+25.0)","",hopt);
-  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_lograte.png");
+  c1.SaveAs(outputdir + "/" + inputname + "_" + "h_lograte.pdf");
 
   // Save and close
   file.Write();
