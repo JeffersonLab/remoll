6cc20dd5f07435d495db3c0e6c051a61241dd7e2
Ciprian Gal <chip.ef@gmail.com>
2020-01-26 10:00:53 -0500

    print less

--- analysis/shieldingAna.C
+++ analysis/shieldingAna.C
@@ -167,7 +167,7 @@ long processOne(string fnm){
 
   long nEntries = t->GetEntries();
   cout<<"\tTotal events: "<<nEntries<<endl;
-  float currentProc=1,procStep=10;
+  float currentProc=1,procStep=30;
   vector<int> procID;
   int sector(-1);
   double pi = acos(-1);
