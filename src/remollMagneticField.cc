874f61c14d2270f4ecf4e9440bbea0b8d0ae363d
Sakib Rahman <rahmans@beluga3.int.ets1.calculquebec.ca>
2020-11-08 12:14:49 -0500

    Changing the magnetic field offset back to 0 to accomodate new field maps which will be in hall coordinates

--- src/remollMagneticField.cc
+++ src/remollMagneticField.cc
@@ -42,7 +42,7 @@ remollMagneticField::remollMagneticField( G4String filename ){
 
     // Default offset for field maps in reference frame with
     // the hall pivot at z = 0.
-    fZoffset = -5000.0;
+    fZoffset = 0.0;
 
     fInit = false;
     fMagCurrent0 = -1e9;
