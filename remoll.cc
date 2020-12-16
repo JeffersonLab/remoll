57934dbbb790e8a17decbeb5f549aeac673c3511
Wouter Deconinck <wdconinc@gmail.com>
2020-12-15 18:06:43 -0600

    [bias] turn off entirely when `-b off`

--- remoll.cc
+++ remoll.cc
@@ -135,13 +135,13 @@ int main(int argc, char** argv) {
 
     // Physics list with biasing
     remollPhysicsList* physlist = new remollPhysicsList();
-    G4GenericBiasingPhysics* biasing = new G4GenericBiasingPhysics();
     if (bias == "on") {
+      G4GenericBiasingPhysics* biasing = new G4GenericBiasingPhysics();
       std::vector<G4String> electronProcessesToBias;
       electronProcessesToBias.push_back("MollerBhabha");
       biasing->PhysicsBias("e-", electronProcessesToBias);
+      physlist->RegisterPhysics(biasing);
     }
-    physlist->RegisterPhysics(biasing);
     runManager->SetUserInitialization(physlist);
 
     // Run action
