df13d486474ce59dd365064840d9c3b2c60b0b7d
Caryn Palatchi <palatchi@ifarm1901.jlab.org>
2021-04-14 17:08:36 -0400

    new batch submission script

--- scripts/sBatchSubmit.py
+++ scripts/sBatchSubmit.py
@@ -4,21 +4,21 @@ import sys, os, time, tarfile
 
 def main():
 
-    email = "ciprian@jlab.org"
+    email = "palatchi@jlab.org"
 
-    config = "moller_TgtConf4"
+    config = "remoll_bellows_beam_100M"
 
-    sourceDir = "/work/halla/moller12gev/ciprian/remoll"
-    outDir = "/volatile/halla/moller12gev/ciprian/"+config
+    sourceDir = "/w/moller12gev-sciwork18/palatchi/moller/remoll/"
+    outDir = "/volatile/halla/moller12gev/palatchi/"+config
 
-    activeDetectors = [28, 55, 56, 57, 58]
+    activeDetectors = [28, 70, 71, 72, 73, 74, 75, 76]
 
     if not os.path.exists(outDir):
        os.makedirs(outDir)
-    nrEv   = 1000 #900000
-    nrStart= 0
-    nrStop = 4 #(nrStop -nrStart) total jobs
-    submit  = 0
+    nrEv   = 100000 #900000 100k is 10hours
+    nrStart= 1
+    nrStop = 1001 #(nrStop -nrStart) total jobs
+    submit  = 1
 
     print('Running ' + str(nrEv*(nrStop - nrStart)) + ' events...')
 
@@ -51,15 +51,16 @@ def createMacFile(outDirFull,nrEv,jobNr, detectorList):
         os.makedirs(outDirFull)
 
     f=open(outDirFull+"/"+"macro.mac",'w')
-    f.write("/remoll/setgeofile geometry/mollerMother_merged.gdml\n")
-    f.write("/remoll/physlist/register QGSP_BERT_HP\n")
-    f.write("/remoll/physlist/parallel/enable\n")
-    f.write("/remoll/parallel/setfile geometry/mollerParallel.gdml\n")
-    #f.write("/run/numberOfThreads 16\n")
+
+    f.write("/remoll/geometry/setfile geometry/mollerMother.gdml\n")
+    f.write("/remoll/parallel/setfile geometry/mollerParallel.gdml \n")
+    f.write("/remoll/physlist/parallel/enable \n")
     f.write("/run/initialize\n")
-    f.write("/remoll/addfield map_directory/hybridJLAB.txt\n")
-    f.write("/remoll/addfield map_directory/upstreamJLAB_1.25.txt\n")
-    f.write("/remoll/evgen/set beam\n")
+    f.write("/remoll/printgeometry true\n")
+    f.write("/remoll/addfield map_directory/V1DSg.3.txt\n")
+    f.write("/remoll/addfield map_directory/V1U.2a.txt\n")
+#?    f.write("/remoll/oldras true\n")
+
     f.write("/remoll/evgen/beam/origin 0 0 -7.5 m\n")
     f.write("/remoll/evgen/beam/rasx 5 mm\n")
     f.write("/remoll/evgen/beam/rasy 5 mm\n")
@@ -68,8 +69,17 @@ def createMacFile(outDirFull,nrEv,jobNr, detectorList):
     f.write("/remoll/evgen/beam/rasrefz -4.5 m\n")
     f.write("/remoll/beamene 11 GeV\n")
     f.write("/remoll/beamcurr 65 microampere\n")
-    f.write("/remoll/SD/disable_all\n")
+#    f.write("/remoll/evgen/set elastic\n")
+    f.write("/remoll/evgen/set beam\n")   
+    f.write("/remoll/evgen/beamPolarization +L\n")
+    f.write("/remoll/field/equationtype 2\n")
+    f.write("/remoll/field/steppertype 2\n")
+    f.write("/remoll/field/print\n")
+    f.write("/remoll/beamene 11 GeV\n")
+    f.write("/remoll/beamcurr 65 microampere\n")
 
+#add from cip job
+    f.write("/remoll/SD/disable_all\n")
     for det in detectorList:
         f.write("/remoll/SD/enable "+str(det)+"\n")
         f.write("/remoll/SD/detect lowenergyneutral "+str(det)+"\n")
@@ -77,10 +87,18 @@ def createMacFile(outDirFull,nrEv,jobNr, detectorList):
         f.write("/remoll/SD/detect boundaryhits "+str(det)+"\n")  
 
     f.write("/remoll/kryptonite/enable\n")
+    f.write("/remoll/kryptonite/add VacuumKryptonite\n")
+    f.write("/remoll/kryptonite/add Tungsten\n")
+    f.write("/remoll/kryptonite/add Copper\n")
+    f.write("/remoll/kryptonite/add Lead\n")
+    f.write("/remoll/kryptonite/add CW95\n")
+    f.write("/remoll/kryptonite/list\n")
     f.write("/process/list\n")
     f.write("/remoll/seed "+str(jobNr)+"\n")
-    f.write("/remoll/filename o_remoll.root\n")
+    f.write("/remoll/filename remollout.root\n")
+    f.write("/remoll/printtargetinfo\n")
     f.write("/run/beamOn "+str(nrEv)+"\n")
+
     f.close()
     return 0
 
@@ -112,14 +130,17 @@ def make_tarfile(sourceDir):
     tar = tarfile.open(sourceDir+"/jobs/default.tar.gz","w:gz")
     tar.add(sourceDir+"/build/remoll",arcname="remoll")
     tar.add(sourceDir+"/build/libremoll.so",arcname="libremoll.so")
-    tar.add(sourceDir+"/map_directory//hybridJLAB.txt",arcname="map_directory/hybridJLAB.txt")
-    tar.add(sourceDir+"/map_directory/upstreamJLAB_1.25.txt",arcname="map_directory/upstreamJLAB_1.25.txt")
+#    tar.add(sourceDir+"/map_directory//hybridJLAB.txt",arcname="map_directory/hybridJLAB.txt")
+#    tar.add(sourceDir+"/map_directory/upstreamJLAB_1.25.txt",arcname="map_directory/upstreamJLAB_1.25.txt")
+    tar.add(sourceDir+"/map_directory/V1DSg.3.txt",arcname="map_directory/V1DSg.3.txt")
+    tar.add(sourceDir+"/map_directory/V1U.2a.txt",arcname="map_directory/V1U.2a.txt")
     tar.add(sourceDir+"/geometry/materials.xml",arcname="geometry/materials.xml")
     tar.add(sourceDir+"/geometry/matrices.xml",arcname="geometry/matrices.xml")
     tar.add(sourceDir+"/geometry/positions.xml",arcname="geometry/positions.xml")
     tar.add(sourceDir+"/geometry/solids/world.xml",arcname="geometry/solids/world.xml")
     tar.add(sourceDir+"/geometry/mollerParallel.gdml" ,arcname="geometry/mollerParallel.gdml") 
-    tar.add(sourceDir+"/geometry/mollerMother_merged.gdml" ,arcname="geometry/mollerMother_merged.gdml") 
+    tar.add(sourceDir+"/geometry/mollerMother.gdml" ,arcname="geometry/mollerMother.gdml") 
+    tar.add(sourceDir+"/geometry/donut/donutConcreteLead.gdml" ,arcname="geometry/donut/donutConcreteLead.gdml") 
     tar.add(sourceDir+"/geometry/target/subTargetRegion.gdml" ,arcname="geometry/target/subTargetRegion.gdml") 
     tar.add(sourceDir+"/geometry/electronics/subSBSbunker.gdml" ,arcname="geometry/electronics/subSBSbunker.gdml") 
     tar.add(sourceDir+"/geometry/hall/hallDaughter_merged.gdml" ,arcname="geometry/hall/hallDaughter_merged.gdml")
@@ -128,11 +149,16 @@ def make_tarfile(sourceDir):
     tar.add(sourceDir+"/geometry/upstream/upstreamDaughter_merged.gdml" ,arcname="geometry/upstream/upstreamDaughter_merged.gdml")
     tar.add(sourceDir+"/geometry/upstream/upstreamToroid.gdml" ,arcname="geometry/upstream/upstreamToroid.gdml")
     tar.add(sourceDir+"/geometry/upstream/upstreamBeampipe.gdml" ,arcname="geometry/upstream/upstreamBeampipe.gdml")
+    tar.add(sourceDir+"/geometry/upstream/sieve.gdml" ,arcname="geometry/upstream/sieve.gdml")
     tar.add(sourceDir+"/geometry/hybrid/hybridToroid.gdml" ,arcname="geometry/hybrid/hybridToroid.gdml")
     tar.add(sourceDir+"/geometry/hybrid/hybridDaughter_merged.gdml" ,arcname="geometry/hybrid/hybridDaughter_merged.gdml")
     tar.add(sourceDir+"/geometry/huts/lefthut.gdml" ,arcname="geometry/huts/lefthut.gdml")
     tar.add(sourceDir+"/geometry/showermax/showerMaxGen.gdml" ,arcname="geometry/showermax/showerMaxGen.gdml")
-    tar.add(sourceDir+"/geometry/pion/pionDetectorSystem.gdml" ,arcname="geometry/pion/pionDetectorSystem.gdml")
+    tar.add(sourceDir+"/geometry/tracking/trackingDaughter.gdml" ,arcname="geometry/tracking/trackingDaughter.gdml")
+    tar.add(sourceDir+"/geometry/pion/" ,arcname="geometry/pion/")
+    tar.add(sourceDir+"/geometry/pion/TS/" ,arcname="geometry/pion/TS/")
+    tar.add(sourceDir+"/geometry/pion/GEM/" ,arcname="geometry/pion/GEM/")
+    tar.add(sourceDir+"/geometry/pion/Lucite/" ,arcname="geometry/pion/Lucite/")
     tar.add(sourceDir+"/geometry/beampipe/downstream/beampipeDSMother.gdml" ,arcname="geometry/beampipe/downstream/beampipeDSMother.gdml")
     tar.add(sourceDir+"/geometry/beampipe/premoller/beampipeRaster.gdml" ,arcname="geometry/beampipe/premoller/beampipeRaster.gdml")
     tar.close()
