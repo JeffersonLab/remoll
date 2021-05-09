d5ef91292539d27448090aba67abfe741869e95c
Zuhal Seyma Demiroglu <zuhal.seyma.demiroglu@cern.ch>
2021-05-09 15:22:46 +0200

    macros from  JeffersonLab:feature-target-shield-design

--- analysis/c1ShldAna.C
+++ analysis/c1ShldAna.C
@@ -15,11 +15,7 @@
 R__LOAD_LIBRARY(radDamage_cc.so)
 #include "radDamage.hh"
 #include "beamLineDetHistos.h"
-#include <iterator>
-#include <iostream>
-#include <vector>
 
-using namespace std;
 TFile *fout;
 string fileNm;
 int beamGen(1);
@@ -29,37 +25,11 @@ long currentEvNr(0);
 
 radDamage radDmg;
 beamLineDetHistos beamLine(3);
-beamLineDetHistos beamLineHole(3);
-beamLineDetHistos beamLineem;
-beamLineDetHistos xDet;
 
 void initHisto();
-void initHisto_detX();
-void initHisto_det5614();
-void fillHisto_detX();
-void fillHisto_det5614();
-void writeOutput_detX();
-void writeOutput_det5614();
-
 void writeOutput();
 long processOne(string);
 void process(int);
-
-struct hitx{
-  int evid;
-  int dID;
-  int tID;
-  int PID;
-  double energy;
-  double time;
-  double posx;
-  double posy;
-  double pZ;
-    
-  hitx(int evid_ = 0, int dID_ = 0,  int tID_ = 0,  double PID_ = 0,  double energy_ = 0,  double time_ = 0,  double posx_ = 0,  double posy_ = 0, double pZ_ = 0) :
-    evid(evid_), dID(dID_), tID(tID_), PID(PID_),
-    energy(energy_), time(time_), posx(posx_), posy(posy_), pZ(pZ_) { };
-};
 
 void c1ShldAna(const string& finName = "./remollout.root", const int testRun=0){
   fileNm = finName;
@@ -104,7 +74,6 @@ long processOne(string fnm){
     delete fin;
     return 0;
   }
-
   TTree *t=(TTree*)fin->Get("T");
   if (t == 0) return 0;
   Double_t rate=0;
@@ -126,17 +95,12 @@ long processOne(string fnm){
   vector<int> procID;
   int sector(-1);
 
-  std::vector<hitx> hits5614;
-  hits5614.clear();
-  hitx bHit;
-
   for (Long64_t event = 0; event < nEntries; t->GetEntry(event++)) {
     currentEvNr++;
     if( float(event+1)/nEntries*100 > currentProc){
       cout<<"at tree entry\t"<<event<<"\t"<< float(event+1)/nEntries*100<<endl;
       currentProc+=procStep;
     }
-   
 
     for(int j=0;j<hit->size();j++){
 
@@ -144,7 +108,6 @@ long processOne(string fnm){
       rate=1; //this is for beam simulations only (right now)
 
       int sp = spM[int(hit->at(j).pid)]-1;
-        
       if(sp==-1) continue;
       double kinE = hit->at(j).k;
 
@@ -192,66 +155,18 @@ long processOne(string fnm){
 	localPZ = -hit->at(j).py;
       }
 
-      if ( det==5614 && hit->at(j).pid==11) {
-	bHit.evid =event;
-	bHit.dID = hit->at(j).det;
-	bHit.tID = hit->at(j).trid;
-	bHit.PID = hit->at(j).pid;
-	bHit.energy = hit->at(j).k;
-	bHit.time = hit->at(j).t;
-	bHit.posx = localXX;
-	bHit.posy = localYY;
-	bHit.pZ = localPZ;
-	hits5614.push_back(bHit);
-      }
-                
+
       beamLine.fillHisto(det, sp, rdDmg, localPZ, localXX, localYY, 
-			 kinE, localRR,vx0,vy0,vz0,0);
-        
-      if ( det==5620 && rr > 265) beamLineHole.fillHisto(det, sp, rdDmg, localPZ, localXX, localYY,
-							 kinE, localRR,vx0,vy0,vz0,0);
+			 kinE, localRR,vx0,vy0,vz0,0);      
 
-      if(det==5614 && hit->at(j).trid==1 && hit->at(j).pid==11) beamLineem.fillHisto_det5614(det, rdDmg, localPZ, localXX, localYY,
-                                                                                             kinE, localRR,vx0,vy0,vz0,0);
+      if(hit->at(j).trid==1 && hit->at(j).mtrid==0 && kinE>1) 
+	beamLine.fillHisto(det, 4, rdDmg, localPZ, localXX, localYY, 
+			   kinE, localRR,vx0,vy0,vz0,0);      
 
-      if(hit->at(j).trid==1 && hit->at(j).mtrid==0 && kinE>1) beamLine.fillHisto(det, 4, rdDmg, localPZ, localXX, localYY,
-										 kinE, localRR,vx0,vy0,vz0,0);      
+      if((sp==0 || sp==5) && kinE>1)
+	beamLine.fillHisto(det, 1, rdDmg, localPZ, localXX, localYY, 
+			   kinE, localRR,vx0,vy0,vz0,0);      
 
-      if((sp==0 || sp==5) && kinE>1) beamLine.fillHisto(det, 1, rdDmg, localPZ, localXX, localYY,
-							kinE, localRR,vx0,vy0,vz0,0);      
- 
-    }
-
-    for(int j=0;j<hit->size();j++){
-      if(std::isnan(rate) || std::isinf(rate)) continue;
-      rate=1; //this is for beam simulations only (right now)
-
-      double kinE = hit->at(j).k;
-      double rdDmg[2] = {rate,rate*kinE};
-      double rr = hit->at(j).r;
-      double xx = hit->at(j).x;
-      double yy = hit->at(j).y;
-      int det = hit->at(j).det;
-      double pz = hit->at(j).pz;
-        
-      if (hit->at(j).det == 28){
-	for (auto &k : hits5614){
-              
-	  double _kinE = k.energy;
-	  double _rr = sqrt(k.posx*k.posx + k.posy*k.posy);
-	  double _rdDmg[2] = {rate,rate*_kinE};
-          double _xx =k.posx;
-	  double _yy =k.posy;
-	  int _det = k.dID;
-	  double _pz = k.pZ;
-              
-	  if (hit->at(j).trid  == k.tID ){
-	    xDet.fillHisto_detX(_det, _rdDmg, _pz, _xx, _yy,_kinE, _rr);
-        if(hit->at(j).pid == 11) xDet.fillHisto_detX(det, rdDmg, pz, xx, yy,kinE, rr);
-	  }
-	}
-
-      }
     }
   }
 
@@ -266,28 +181,25 @@ void initHisto(){
 
   fout = new TFile(foutNm.c_str(),"RECREATE");
 
-  // void beamLineDetHistos::initHisto(TFile *fout, const int detID, const string detNm, 
-  // 				  const float vxRgMin , const float vxRgMax,
-  // 				  const float vyRgMin , const float vyRgMax, 
-  // 				  const float vzRgMin , const float vzRgMax,
-  // 				  const string postfix = "", const float range=2000, const int subDet=0)
-  beamLine.initHisto(fout,5620,"After C4PbWall"    ,-2500,2500,-2500,2500,-5600,6000,"",2500);
-  beamLine.initHisto(fout,5543,"After Tgt bunker"  ,-2500,2500,-2500,2500,-5600,6000,"",2500);
+// void beamLineDetHistos::initHisto(TFile *fout, const int detID, const string detNm, 
+// 				  const float vxRgMin , const float vxRgMax,
+// 				  const float vyRgMin , const float vyRgMax, 
+// 				  const float vzRgMin , const float vzRgMax,
+// 				  const string postfix = "", const float range=2000, const int subDet=0)
+  beamLine.initHisto(fout,5620,"After C4PbWall"    ,-2500,2500,-2500,2500,-5600,4000,"",2500);
+  beamLine.initHisto(fout,5543,"After Tgt bunker"  ,-2500,2500,-2500,2500,-5600,4000,"",2500);
 
-  beamLine.initHisto(fout,5600,"c1 In  Beam Right" ,-2500,2500,-2500,2500,-5600,6000,"",2500);
-  beamLine.initHisto(fout,5610,"c1 Out Beam Right" ,-2500,2500,-2500,2500,-5600,6000,"",2500);
-  beamLine.initHisto(fout,5601,"c1 In  Beam Left"  ,-2500,2500,-2500,2500,-5600,6000,"",2500);
-  beamLine.initHisto(fout,5611,"c1 Out Beam Left"  ,-2500,2500,-2500,2500,-5600,6000,"",2500);
+  beamLine.initHisto(fout,5600,"c1 In  Beam Right" ,-2500,2500,-2500,2500,-5600,4000,"",2500);
+  beamLine.initHisto(fout,5610,"c1 Out Beam Right" ,-2500,2500,-2500,2500,-5600,4000,"",2500);
+  beamLine.initHisto(fout,5601,"c1 In  Beam Left"  ,-2500,2500,-2500,2500,-5600,4000,"",2500);
+  beamLine.initHisto(fout,5611,"c1 Out Beam Left"  ,-2500,2500,-2500,2500,-5600,4000,"",2500);
 
-  beamLine.initHisto(fout,5613,"c1 Out Top"        ,-2500,2500,-2500,2500,-5600,6000,"",2500);
-  beamLine.initHisto(fout,5603,"c1 In  Top"        ,-2500,2500,-2500,2500,-5600,6000,"",2500);
-  beamLine.initHisto(fout,5614,"c1 Out Bottom"     ,-2500,2500,-2500,2500,-5600,6000,"",2500);
-    
-  beamLineHole.initHisto(fout,5620,"After C4PbWall with Hole"     ,-2500,2500,-2500,2500,-5600,6000,"hole",2500);
-  beamLineem.initHisto_det5614(fout,5614,"c1 Out Bottom trackid==1","tr",2500);
+  beamLine.initHisto(fout,5613,"c1 Out Top"        ,-2500,2500,-2500,2500,-5600,4000,"",2500);
+  beamLine.initHisto(fout,5603,"c1 In  Top"        ,-2500,2500,-2500,2500,-5600,4000,"",2500);
+  beamLine.initHisto(fout,5614,"c1 Out Bottom"     ,-2500,2500,-2500,2500,-5600,4000,"",2500);
 
-  xDet.initHisto_detX(fout,28,"Main detector for only electrons");
-  xDet.initHisto_detX(fout,5614,"c1 Out Bottom detector for only electrons");
+
+
 
 }
 
@@ -306,12 +218,6 @@ void writeOutput(){
   beamLine.writeOutput(fout,5611,scaleFactor);
   beamLine.writeOutput(fout,5613,scaleFactor);
   beamLine.writeOutput(fout,5614,scaleFactor);
-    
-  beamLineHole.writeOutput(fout,5620,scaleFactor);
-  beamLineem.writeOutput_det5614(fout,5614,scaleFactor);
- 
-  xDet.writeOutput_detX(fout,28,scaleFactor);
-  xDet.writeOutput_detX(fout,5614,scaleFactor);
 
   fout->Close();
 }
