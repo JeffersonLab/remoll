3d83d3c0d203fb9ddd901eab0205d6a1f91520fe
Sakib Rahman <rahmans@myumanitoba.ca>
2023-03-06 13:14:37 -0600

    Optimal resource requirements on beluga

--- scripts/standalone_edep_studies/submitBelugaJobs.sh
+++ scripts/standalone_edep_studies/submitBelugaJobs.sh
@@ -5,8 +5,8 @@
 #SBATCH --time=00:30:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=5
-#SBATCH --mem=10G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16GB
 #SBATCH --output=/lustre03/project/6049248/commonFiles/log/%A_%a.out
 #SBATCH --error=/lustre03/project/6049248/commonFiles/log/%A_%a.err
 
@@ -14,8 +14,8 @@ cp -r /lustre03/project/6049248/commonFiles/remoll $TMPDIR
 
 cd $TMPDIR/remoll
 
-python scripts/standalone_edep_studies/generateStandalone.py -c scripts/standalone_edep_studies/DSconfig.list
+python scripts/standalone_edep_studies/generateStandalone.py -c scripts/standalone_edep_studies/USconfig.list
 
 ./build/remoll macros/run.mac
 
-mv  DSconfig.root /lustre03/project/6049248/commonFiles/out/DSconfig_${SLURM_JOBID}_${SLURM_ARRAY_TASK_ID}.root
+mv  USconfig.root /lustre03/project/6049248/commonFiles/out/USconfig1_${SLURM_JOBID}_${SLURM_ARRAY_TASK_ID}.root
